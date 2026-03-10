7cfdc66148913c0576424716f2559d399054aded
Alesh <alexadin30@gmail.com>
2026-03-10 08:57:28 +0000

    Please bro

--- src/Route.java
+++ src/Route.java
@@ -12,28 +12,51 @@ public class Route {
 
     private ArrayList<Ponto> pontos;
 
+    /**
+     * Constrói uma nova instância da classe Route com uma lista de pontos especificada.
+     *
+     * @param pontos Uma lista de objetos {@code Ponto} que representam os pontos sequenciais
+     *               que compõem a rota.
+     * @pre ArrayList pontos não vazio
+     * @post Uma instância valida de Route
+     */
     Route(ArrayList<Ponto> pontos) {
         this.pontos = pontos;
     }
 
+    /**
+     * Identifica e processa os pontos de interseção entre os segmentos da rota e um
+     * dado {@code SegmentoReta}. O resultado é impresso, mostrando os pontos de
+     * interseção ou indicando que não há interseções.
+     *
+     * @param sg O {@code SegmentoReta} com o qual os segmentos da rota serão
+     *           testados para interseção.
+     * @pre sg valido
+     *
+     */
     public void intersect(SegmentoReta sg) {
 
         boolean exit = false;
+        ArrayList<Ponto> pontosIntersect = new ArrayList<>();
+
         for (int i = 1; i < pontos.size(); i++) {
             SegmentoReta temp = new SegmentoReta(pontos.get(i - 1), pontos.get(i));
             Ponto intersect = temp.intersect(sg);
             if (intersect != null) {
-                IO.print(intersect);
+                pontosIntersect.add(intersect);
                 exit = true;
-                if (i == pontos.size() - 1) {
-                    IO.print("\n");
-                } else {
-                    IO.print(" ");
-                }
             }
         }
         if (!exit) {
             IO.println("null");
+        } else {
+            for (int i = 0; i < pontosIntersect.size(); i++) {
+                IO.print(pontosIntersect.get(i).toString());
+                if (i < pontosIntersect.size() - 1) {
+                    IO.print(" ");
+                }
+            }
+            IO.print("\n");
         }
 
     }
